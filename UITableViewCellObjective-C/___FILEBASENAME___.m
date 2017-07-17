1ffa0e3cd9edcfa56db855f1f77c01b6c0e11f74
NickHao <491493118@qq.com>
2017-07-17 16:28:17 +0800

    路径 xcode 8 ：/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File Templates/Source
    路径 xcode 8
    ：/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/
    Developer/Library/Xcode/Templates/File Templates/Source

---
+++ UITableViewCellObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,46 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        self.backgroundColor = [UIColor whiteColor];
+        
+        [self setUpLayout];
+        //        [self hy_setAllSubviewsBorderRed];
+    }
+    return self;
+}
+
+
+- (void)setUpLayout{
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ UITableViewCellXIBObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,32 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
